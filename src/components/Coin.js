2a1c4a631db6d6f155cd6806bc70174c1cee5d99
asmafrija <asmafrija@hotmail.fr>
2022-02-06 03:50:26 -0800

    mon deuxieme  commit

---
+++ src/components/Coin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Coin = ({ name, icon, price, symbol }) => {
+    return (<div className='coin'>
+        <h1> Name:{name}</h1>
+        <img src={icon} />
+        <h3> price:{price}</h3>
+        <h3> Symbol:{symbol}</h3>
+    </div>);
+};
+
+export default Coin;
